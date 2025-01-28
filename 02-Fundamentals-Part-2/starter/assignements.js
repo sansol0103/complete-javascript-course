e8c2eed8b7118c3f148554fc3ca7dafaf54af72d
sansol0103 <sansol_01@pm.me>
2025-01-28 14:41:46 +0100

    Did more stuff

---
+++ 02-Fundamentals-Part-2/starter/assignements.js
@@ -0,0 +1,59 @@
+// function describeCountry(country, population, capitalCity) {
+//     return (`${country} has a population of ${population} million people and its capital city is ${capitalCity}`);
+// };
+
+// const describeNorway = describeCountry('Norway', 5, 'Oslo');
+// const describeUkraine = describeCountry('Ukraine', 37, 'Kyiv');
+// const describeIreland = describeCountry('Ireland', 4, 'Dublin');
+
+// console.log(describeNorway);
+// console.log(describeUkraine);
+// console.log(describeIreland);
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+
+// const percentageOfWorldNorway = percentageOfWorld1(5);
+// const percentageOfWorldUkraine = percentageOfWorld1(37);
+// const percentageOfWorldIreland = percentageOfWorld1(4);
+
+// console.log(percentageOfWorldNorway);
+// console.log(percentageOfWorldUkraine);
+// console.log(percentageOfWorldIreland);
+
+// const percentageOfWorld2 = function(population) {
+//     return (population / 7900) * 100;
+// }
+
+// const percentageOfWorldPhilipines = percentageOfWorld2(117);
+// const percentageOfWorldUK = percentageOfWorld2(68);
+// const percentageOfWorldSweden = percentageOfWorld2(10);
+
+// console.log(percentageOfWorldPhilipines);
+// console.log(percentageOfWorldUK);
+// console.log(percentageOfWorldSweden);
+
+// const percentageOfWorld3 = population => (population / 7900) * 100;
+
+// const percentageOfWorldNorway = percentageOfWorld3(5);
+// const percentageOfWorldUkraine = percentageOfWorld3(37);
+// const percentageOfWorldPhilipines = percentageOfWorld3(117);
+
+// console.log(percentageOfWorldNorway);
+// console.log(percentageOfWorldUkraine);
+// console.log(percentageOfWorldPhilipines);
+
+// function describePopulation(country, population) {
+//     const precentage = percentageOfWorld1(population);
+//     const description = `${country} has a population of ${population} million, which is about ${precentage} of the worlds total population`;
+//     return description;
+// };
+
+// const NorwayDescribed = describePopulation('Norway', 6);
+// const UkraineDescribed = describePopulation('Ukraine', 37);
+// const UKDescribed = describePopulation('UK', 68);
+
+// console.log(NorwayDescribed);
+// console.log(UkraineDescribed);
+// console.log(UKDescribed);
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,106 @@
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log(`I can drive!`);
+
+// const interface = 'Audio';
+// const private = true;
+// const if = 23;
+
+
+function logger() {
+    console.log(`My name is Sander`);
+}
+
+// calling / running / invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+};
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+console.log(num);
+
+
+// function decleration. Can call the function before its declared
+function calcAge1(birthYear) {
+    return 2025 - birthYear;
+}
+
+const myAge = calcAge1(2001);
+console.log(myAge);
+
+// function expression. Cannot call the function before ts declared
+const calcAge2 = function (birthYear) {
+    return 2025 - birthYear;
+}
+
+const otherAge = calcAge2(1998);
+
+console.log(myAge, otherAge);
+
+// Arrow function
+const calcAge3 = birthYear => 2025 - birthYear;
+
+const myAge = calcAge3(2001);
+console.log(myAge);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2025 - birthYear;
+    const retirement = 70 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(2001, 'Sander'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+
+function cutter(fruit) {
+    return fruit * 4;
+};
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutter(apples);
+    const orangePieces = cutter(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+};
+
+console.log(fruitProcessor(2,3));
+
+
+const calcAge = function (birthYear) {
+    return 2025 - birthYear;
+}
+
+const yearsUntilRetirement = function(birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 70 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
+    // return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(2001, 'Sander'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+*/ 
